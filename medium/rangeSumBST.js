afe8f742ccdfbe7a65685b5f593f9a4bee1a2674
Joseph-Kung <joseph.kung@hotmail.com>
2019-01-27 15:07:26 -0800

    Complete rangeSumBST

---
+++ medium/rangeSumBST.js
@@ -0,0 +1,36 @@
+// /**
+//  * Definition for a binary tree node.
+//  * function TreeNode(val) {
+//  *     this.val = val;
+//  *     this.left = this.right = null;
+//  * }
+//  */
+// /**
+//  * @param {TreeNode} root
+//  * @param {number} L
+//  * @param {number} R
+//  * @return {number}
+//  */
+
+//O(log n) time
+
+var rangeSumBST = function (root, L, R) {
+  let stack = [root];
+  let sum = 0;
+
+  while (stack.length > 0) {
+    let node = stack.pop();
+    if (node === null) continue;
+    if (node.val >= L && node.val <= R) {
+      sum += node.val;
+      stack.push(node.right);
+      stack.push(node.left);
+    } else if (node.val >= R) {
+      stack.push(node.left);
+    } else if (node.val <= L) {
+      stack.push(node.right);
+    }
+  }
+
+  return sum;
+};
